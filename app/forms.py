3ac88b7c47b26539c3f693b7b06a7e304c6db91d
Denys Badzo <badzyo360@gmail.com>
2016-06-20 00:30:23 +0300

    Game creation view and games list at homepage

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Length, EqualTo, Email
 from app.validators import UniqueUserNameValidator, UniqueEmailValidator
 
@@ -16,3 +16,20 @@ class RegisterForm(Form):
                              validators=[Length(min=3, max=20),
                                          EqualTo("password_check", message="Passwords don't match")])
     password_check = PasswordField('Repeat password', validators=[Length(min=3, max=20)])
+
+
+class NewGameForm(Form):
+    sizes = [(3, '3x3'),
+             (4, '4x4'),
+             (5, '5x5'),
+             (6, '6x6'),
+             (7, '7x7'),
+             (8, '8x8'),
+             (9, '9x9'),
+             (10, '10x10')]
+
+    rules = [(3, '3 in row'),
+             (4, '4 in row'),
+             (5, '5 in row')]
+    size = SelectField('Size of the game field', default=3, choices=sizes, coerce=int)
+    rule = SelectField('Winning rule', default=3, choices=rules, coerce=int)
--- app/views.py
+++ app/views.py
@@ -1,15 +1,18 @@
+import random
 from flask import render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, login_manager
-from app.models import User
+from app.models import User, Game, GameUser, GameMove
 from app import forms
 
 
 @app.route("/")
 @login_required
 def index():
-    user = current_user
-    return render_template('index.html', user=user)
+    games_in_wait = Game.query.filter_by(state=Game.game_state['waiting_for_players']).limit(5)
+    games_in_progress = Game.query.filter(Game.state.in_([Game.game_state['player_one_turn'],
+                                                          Game.game_state['player_two_turn']])).limit(5)
+    return render_template('index.html', games_in_progress=games_in_progress, games_in_wait=games_in_wait)
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -53,6 +56,34 @@ def register():
         return redirect(url_for('index'))
 
     return render_template('register.html', register_form=form)
+
+
+@app.route("/game/new", methods=['GET', 'POST'])
+@login_required
+def new_game():
+    if request.method == 'POST':
+        form = forms.NewGameForm(request.form)
+    else:
+        form = forms.NewGameForm()
+
+    if form.validate_on_submit():
+        game = Game(field_size=form.size.data, win_length=form.rule.data)
+        db.session.add(game)
+        user_order = random.choice([GameUser.user_game_role['player_one'],
+                                    GameUser.user_game_role['player_two']])
+        game_user = GameUser(user=current_user, game=game, user_role=user_order)
+        db.session.add(game_user)
+        db.session.commit()
+        return redirect(url_for('show_game', game_id=game.id))
+
+    return render_template('new_game.html', new_game_form=form)
+
+
+@app.route("/game/<int:game_id>", methods=['GET'])
+@login_required
+def show_game(game_id):
+    game = Game.query.get_or_404(game_id)
+    return render_template('game.html', game=game)
 
 
 @login_manager.user_loader
